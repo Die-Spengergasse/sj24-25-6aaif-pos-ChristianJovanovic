4bb346762b499c125f308dd4abfa5644cdba29ea
ChristianJovanovic <christianjovanovic0708@gmail.com>
2025-03-27 15:44:39 +0100

    implements UpdatePaymentItemCmd

--- 02_get_requests/Asp_Wiederholung_6AAIF/src/SPG_Fachtheorie.Aufgabe3/Commands/NewPaymentCmd.cs
+++ 02_get_requests/Asp_Wiederholung_6AAIF/src/SPG_Fachtheorie.Aufgabe3/Commands/NewPaymentCmd.cs
@@ -3,7 +3,7 @@ using SPG_Fachtheorie.Aufgabe1.Model;
 
 namespace SPG_Fachtheorie.Aufgabe3.Commands;
 
-//TODO: Die Validierung von paymentDateTime überarbeiten. Derzeit nicht funktional
+
 public record NewPaymentCmd(
     int cashDeskNumber,
     DateTime paymenDateTime,
